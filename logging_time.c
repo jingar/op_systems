c982204224dd0d349f506670bf591300edb11a10
Saad Mehmood Arif <sa521@linux13.macs.hw.ac.uk>
2013-01-16 13:33:07 +0000

    Problem 2- Added 10 second delay

--- logging_time.c
+++ logging_time.c
@@ -5,20 +5,24 @@
   {
     time_t lt;
 	FILE *out;
-    lt = time(NULL);
     
-	out = fopen( "s_k_log.log", "w" );
-	if( out != NULL )
-	  fprintf( out, "Time is %s\n", ctime(&lt) );
-	
-	fclose(out);
-
-    
-    //openlog("Saad and Kos log",LOG_CONS | LOG_PID | LOG_NDELAY,0);
+    while(1)
+	{
+		out = fopen( "s_k_log.log", "a" );
+		if( out != NULL )
+		{	
+			lt = time(NULL);
+			fprintf( out, "Time is %s\n", ctime(&lt) );
+		}
+		fclose(out);
+		
+	//openlog("Saad and Kos log",LOG_CONS | LOG_PID | LOG_NDELAY,0);
 	
     //syslog(LOG_INFO,ctime(&lt));
     
     //closelog();
-	
+		sleep(10);
+	}
+    
     return 0;
   }
