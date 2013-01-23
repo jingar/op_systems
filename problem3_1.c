933d0147e3f112a80b66e5701f84d636d4f694df
Konstantin Devyatov <kd79@linux22.macs.hw.ac.uk>
2013-01-23 17:07:36 +0000

    Saad add me as a contributor

--- problem3_1.c
+++ problem3_1.c
@@ -4,7 +4,16 @@
 #include <sys/types.h>
 
 int countLines(char* buf)
-{  
+{ int i,counter=0;
+  char a;
+  while((a=buf[i]) != '\0')
+  { if(a=='\n')
+    { counter++;  
+    }
+    i++;
+  }
+  return counter;
+}
 
 int main(int argc, char** argv)
 {   FILE* fp = NULL;
@@ -16,13 +25,14 @@ int main(int argc, char** argv)
     int fd,numOfLines = 0;
 
 
-    fp = popen("ls *", "r");
+    fp = popen("ls", "r");
     fd= fileno(fp);
 
     if(fstat(fd, &st) >= 0) {
         bufsz = (size_t) st.st_blksize;
     } else {
 	printf("error 1");
+	exit(0);
     }
 
     //	Allocate buffer of size BUFSZ.
@@ -35,20 +45,27 @@ int main(int argc, char** argv)
     {	curpos+=ssz;
 	cursz=curpos+bufsz;
 	if(NULL == (bufp = (char *) realloc (buf, cursz)))
-	    printf("error 2");
+	{ printf("error 2");
+	  exit(0);
+	}
 	buf = bufp;
     }
 
     // Zero-terminate BUF.
-    buf[curpos] = 0;
+    buf[curpos] = '\0';
     pclose(fp);
 
-    numOfLines = countLines(*buf);
-    if(numbOfLines > 25)
-    {	printf("too many lines");
-    ]
+    numOfLines = countLines(buf);
+    if(numOfLines > 25)
+    { //printf("too many lines");
+      fp = popen("less", "w");
+      fprintf(fp, "%s",buf);
+    }
     else
-    {	printf(buf);
+    { //printf(buf);
+      fp = popen("less", "w");
+      fprintf(fp, "%s",buf);
+      pclose(fp);  
     }
 
     return 0;
