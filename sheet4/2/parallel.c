161b09a95c9804852e376310df7442bfcc86337d
saad <sa521@hw.ac.uk>
2013-03-18 19:08:55 +0000

    Got it working for 1 thread

--- sheet4/2/parallel.c
+++ sheet4/2/parallel.c
@@ -13,25 +13,28 @@
 #include <pthread.h>
 
 #define SIZE 1000
-#define N_INTERATIONS 100000
+#define N_ITERATIONS 100000
+#define N_THREADS 16
 
 struct timespec start, finish;
   
-typedef struct _thread_args{
+typedef struct t_info{
   pthread_t id;
   int from;
   int to;
 }thread_args;
+//typedef struct t_info thread_args;
 
 thread_args** thread_args_ptr;
-//typedef thread_args **thread_args_ptr;
 
-int a[SIZE] = {[0 ... 999] = 1};
-int b[SIZE] = {[0 ... 999] = 1};
+
+int a[SIZE];
+int b[SIZE];
 int first = 0;
 int rest = 0;
 int count_test_par =0;
 int count_test_seq =0;
+
 
 /* Time difference between a and b in microseconds, taken from bodo's coursework*/
 static int64_t
@@ -40,33 +43,35 @@ xelapsed (struct timespec a, struct timespec b)
   return ((int64_t)a.tv_sec - b.tv_sec) * 1000000
     + ((int64_t)a.tv_nsec - b.tv_nsec) / 1000LL;
 }
-void add_arrays_seq()
+void* add_arrays_seq()
 {
   int i,j;
-  for(j = 0; j < 100000;j++)
+  for(j = 0; j < N_ITERATIONS;j++)
     {
       for(i = 0; i < SIZE;i++)
 	{
 	  a[i] = a[i] + b[i];    
 	  count_test_seq++;
 	}
-    }
+  
+  }
 }
-void* add_arrays(void* args)
+void *add_arrays(void* args)
 {
   //int* i = args;
   //printf("add array arg %d \n",*i);
   //printf("add arrays_seq from %d to %d\n",from,to);
   //initialise a and by to size 1000 and make each element equal to 1
-  int i = ((thread_args*)args)->from;
+  printf("IN ADD ARRAYS id %lu, from %d, to %d \n",((thread_args*)args)->id,((thread_args*)args)->from,((thread_args*)args)->to);
+  int from = ((thread_args*)args)->from;
   int to = ((thread_args*)args)->to;
-  int j;
-  for(j = 0; j < 1000000;j++)
+  int i,j;
+  for(j = 0; j < N_ITERATIONS;j++)
     {
-      for(i; i < to;i++)
+      for(i=from; i <= to;i++)
 	{
 	  a[i] = a[i] + b[i];    
-	  count_test_par++;
+	  //printf("a[%d] = %d \n",i,a[i]);
 	}
     }
 }
@@ -77,50 +82,57 @@ void split_work(int number)
   int counter = 0;
   div_t n = div(SIZE,number);
   thread_args_ptr[0]->from = 0;
+  //printf(" SIZE %d, number %d,quot %d , rem %d \n",SIZE,number,n.quot,n.rem);
   if(n.rem >0)
     {
-      counter = thread_args_ptr[0]->to = (n.quot + n.rem)-1;
+      counter = thread_args_ptr[0]->to = (n.quot + n.rem);
+      //printf("counter if rem > 0 %d \n",counter);
     }
   else
     {
       counter = thread_args_ptr[0]->to = n.quot-1;
+      //printf("counter else %d \n",counter);
     }
-  //printf("from %d to %d \n",thread_args_ptr[0]->from ,thread_args_ptr[0]->to);
-  for(i = 1; i < number; i++)
+  printf("first from %d to %d \n", thread_args_ptr[0]->from,thread_args_ptr[0]->to);
+  /*for(i = 1; i < number; i++)
     {
       thread_args_ptr[i]->from = counter+1;
       thread_args_ptr[i]->to = counter + n.quot;
+      //thread_args_ptr[i]->from = thread_args_ptr[i-1]->to;
+      //thread_args_ptr[i]->to = thread_args_ptr[i]->from + n.quot;
       printf(" from %d to %d \n", thread_args_ptr[i]->from,thread_args_ptr[i]->to);
       counter+=n.quot;
-    }
+    n  }*/
 }
 
 void parallel_add(int number_of_threads)
 {
+  printf("START PARRALEL \n");
   int thread_check;
   int i = 0;
-  
+
   thread_args_ptr = malloc(number_of_threads * sizeof(thread_args*));
-  for (i = 0 ; i<number_of_threads ; i++) 
+  for (i = 0 ; i<number_of_threads; i++)
     {
       thread_args_ptr[i] = malloc(sizeof(thread_args));
+      printf("START ALLOCATION MEMORY \n");
     }
-
   if (thread_args_ptr == NULL)
     {
       printf("ERROR: Out of memory\n");
       exit(1);
     }  
-
+  
   split_work(number_of_threads);
+  printf("AFTER SPLIT \n");
   thread_check = pthread_create(&thread_args_ptr[0]->id,NULL,add_arrays,thread_args_ptr[0]);
+  printf("args id %lu first from %d to %d \n", thread_args_ptr[0]->id,thread_args_ptr[0]->from,thread_args_ptr[0]->to);
+  printf("thread_check %d \n",thread_check);
   if(thread_check != 0)
     {
       printf("Thread could not be created");
     }
-
-
-  for (i = 1;i < number_of_threads; i++)
+  /*for (i = 1;i < number_of_threads; i++)
     {     
       thread_check = pthread_create(&thread_args_ptr[i]->id,NULL,add_arrays,thread_args_ptr[i]);
 
@@ -129,14 +141,20 @@ void parallel_add(int number_of_threads)
 	  printf("Thread could not be created");
 	}
       //printf("%lu test \n",thread_args_ptr[i]->id);
-    }
-  for(i=0; i>number_of_threads; i++)
+      }
+ 
+  */
+  for(i=0; i<number_of_threads; i++)
     { 
       if(pthread_join(thread_args_ptr[i]->id, NULL) != 0) 
 	{ 
 	  perror("Thread not created\n");
 	  exit(1);
 	} 
+    }
+  for(i=0; i<number_of_threads; i++) 
+    { 
+      free(thread_args_ptr[i]); 
     }
   free(thread_args_ptr);
 }
@@ -146,8 +164,19 @@ void print_array()
   int i;
   for(i=0; i<SIZE;i++)
     {
-      printf("%d %d",i,a[i]);
+      printf("a %d b %d \n",a[i],b[i]);
     }
+}
+
+
+void init_data( )
+{
+  int i;
+  for( i=0; i<SIZE; i++) {
+    a[i] = i;
+    b[i] = (SIZE-1) - i;
+  }
+  //pthread_barrier_init(&barrier, NULL, N_THREADS);
 }
 
 int main(int argc, char** argv)
@@ -155,14 +184,14 @@ int main(int argc, char** argv)
   if(!(argc < 2))
     {
       int number_of_threads = atoi(argv[1]);
+      init_data();
       printf("%d \n",number_of_threads);
       clock_gettime(CLOCK_REALTIME, &start);
-      
       parallel_add(number_of_threads);
-
+      
       clock_gettime(CLOCK_REALTIME, &finish);
       fprintf (stderr, "Total time: %03li\n", xelapsed (finish, start));
-
+      
       clock_gettime(CLOCK_REALTIME, &start);
       
       add_arrays_seq(number_of_threads);
@@ -171,10 +200,10 @@ int main(int argc, char** argv)
       fprintf (stderr, "Total time: %03li\n", xelapsed (finish, start));
     }
   else{
-    //printf("must be at least one thread \n");
+    printf("must be at least one thread \n");
   }
-  printf("COUNT TEST %d \n",count_test_seq);
-  printf("COUNT TEST %d \n",count_test_par);
+  //printf("COUNT TEST SEQ %d \n",count_test_seq);
+  //printf("COUNT TEST PAR %d \n",count_test_par);
 
   //print_array();
 
